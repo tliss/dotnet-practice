00cc1991c02855a53ae73dbcecdcd39a66f94deb
Taylor A. Liss <tayloraliss@gmail.com>
2021-02-20 00:04:18 -0500

    Add tax calculator functions

--- Chapter04/WritingFunctions/Program.cs
+++ Chapter04/WritingFunctions/Program.cs
@@ -37,9 +37,67 @@ namespace WritingFunctions
             while (isNumber);
         }
 
+        static decimal CalculateTax(decimal amount, string twoLetterRegionCode)
+        {
+            decimal rate = 0.0M;
+            switch (twoLetterRegionCode)
+            {
+                case "CH": // Switzerland
+                    rate = 0.08M;
+                    break;
+                case "DK": // Denmark
+                case "NO": // Norway
+                    rate = 0.25M;
+                    break;
+                case "GB": // United Kingdom
+                case "FR": // France
+                    rate = 0.2M;
+                    break;
+                case "HU": // Hungary
+                    rate = 0.27M;
+                    break;
+                case "OR": // Oregon
+                case "AK": // Alaska
+                case "MT": // Montana
+                    rate = 0.0M;
+                    break;
+                case "ND": // North Dakota
+                case "WI": // Wisconsin
+                case "ME": // Maryland
+                case "VA": // Virginia
+                    rate = 0.05M;
+                    break;
+                case "CA": // California
+                    rate = 0.0825M;
+                    break;
+                default: // most US states
+                    rate = 0.06M;
+                    break;
+            }
+            return amount * rate;
+        }
+
+        static void RunCalculateTax()
+        {
+            Write("Enter an amount: ");
+            string amountInText = ReadLine();
+            Write("Enter a two-letter region code: ");
+            string region = ReadLine();
+            if (decimal.TryParse(amountInText, out decimal amount))
+            {
+                decimal taxToPay = CalculateTax(amount, region);
+                WriteLine($"You must pay {taxToPay} in sales tax.");
+            }
+            else
+            {
+                WriteLine("You did not enter a valid amount!");
+            }
+        }
+
         static void Main(string[] args)
         {
-            RunTimesTable();
+            // RunTimesTable();
+            RunCalculateTax();
         }
     }
 }
