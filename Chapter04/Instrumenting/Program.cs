69b58ebe813ab23135f8ba74a84d9a6db5037ec4
Liss <Taylor.Liss.CTR@ad.dot.gov>
2021-02-25 10:28:10 -0500

    Trace and debug

---
+++ Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace Instrumenting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // write to a text file in the project folder
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt")));
+
+            // text writer is buffered, so this option calls Flush() on all listene
+            Trace.AutoFlush = true;
+
+            Debug.WriteLine("Debug says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+        }
+    }
+}
