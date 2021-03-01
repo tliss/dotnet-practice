57dfe51b8b6b43e4be826da7d1ce7025689d66eb
Liss <Taylor.Liss.CTR@ad.dot.gov>
2021-03-01 18:33:19 -0500

    Remove files

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -16,6 +16,23 @@ namespace Instrumentinga
 
             Debug.WriteLine("Debug says, I am watching!");
             Trace.WriteLine("Trace says, I am watching!");
+
+            var builder = new ConfigurationBuilder().SetBasePath(
+                Directory.GetCurrentDirectory()).AddJsonFile(
+                    "appsettings.json",
+                    optional: true, 
+                    reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var ts = new TraceSwitch(displayName: "PacktSwitch", description: "This switch is set via a JSON config.");
+
+            configuration.GetSection("PacktSwitch").Bind(ts);
+
+            Trace.WriteLineIf(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
         }
     }
 }
