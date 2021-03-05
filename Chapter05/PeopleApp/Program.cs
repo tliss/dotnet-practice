ddf81f344fed4cbda954b5eab2c0a8ef087fb1e6
Taylor Liss <tayloraliss@gmail.com>
2021-03-04 23:19:58 -0500

    Add person class

---
+++ Chapter05/PeopleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Packt.Shared;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bob = new Person();
+            WriteLine(bob.ToString());
+        }
+    }
+}
