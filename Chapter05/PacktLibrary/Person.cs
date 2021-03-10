602f280ee63ed24acd64dd9b2fc42de8f9cf5797
Taylor A. Liss <tayloraliss@gmail.com>
2021-03-09 23:31:19 -0500

    Create new objects

--- Chapter05/PacktLibrary/Person.cs
+++ Chapter05/PacktLibrary/Person.cs
@@ -4,5 +4,8 @@ namespace Packt.Shared
 {
     public class Person : object
     {
+        // fields
+        public string Name;
+        public DateTime DateOfBirth;
     }
 }
--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Packt.Shared;
 using static System.Console;
+using System;
 
 namespace PeopleApp
 {
@@ -8,7 +9,24 @@ namespace PeopleApp
         static void Main(string[] args)
         {
             var bob = new Person();
-            WriteLine(bob.ToString());
+            bob.Name = "Bob Smith";
+            bob.DateOfBirth = new DateTime(1965, 12, 22);
+
+            WriteLine(
+                format: "{0} was born on {1:dddd, d MMMM yyyy}",
+                arg0: bob.Name,
+                arg1: bob.DateOfBirth);
+
+            var alice = new Person
+            {
+                Name = "Alice Jones",
+                DateOfBirth = new DateTime(1998, 3, 7)
+            };
+
+            WriteLine(
+                format: "{0} was born on {1:dd MMM yy}",
+                arg0: alice.Name,
+                arg1: alice.DateOfBirth);
         }
     }
 }
