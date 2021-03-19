971b513f4b7d188e1135b92515b54736143f0c9d
Taylor A. Liss <tayloraliss@gmail.com>
2021-03-18 20:15:12 -0400

    Add pattern matching

--- Chapter05/PeopleApp/Program.cs
+++ Chapter05/PeopleApp/Program.cs
@@ -161,6 +161,29 @@ namespace PeopleApp
             // accessing using new indexers
             WriteLine($"Sam's first child is {sam[0].Name}");
             WriteLine($"Sam's second child is {sam[1].Name}");
+
+            object[] passengers = {
+                new FirstClassPassenger { AirMiles = 1_419 },
+                new FirstClassPassenger { AirMiles = 16_562 },
+                new BusinessClassPassenger(),
+                new CoachClassPassenger { CarryOnKG = 25.7 },
+                new CoachClassPassenger { CarryOnKG = 0 },
+            };
+            
+            foreach (object passenger in passengers)
+            {
+                decimal flightCost = passenger switch
+                {
+                    FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
+                    FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+                    FirstClassPassenger _ => 2000M,
+                    BusinessClassPassenger _ => 1000M,
+                    CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+                    CoachClassPassenger _ => 650M,
+                    _ => 800M
+                };
+                WriteLine($"Flight costs {flightCost:C} for {passenger}");
+            }
         }
     }
 }
