7f2d44b674510620431b8924279a919afac74572
Liss <Taylor.Liss.CTR@ad.dot.gov>
2021-03-25 14:15:56 -0400

    Add instance and static methods

--- Chapter06/PacktLibrary/Person.cs
+++ Chapter06/PacktLibrary/Person.cs
@@ -10,11 +10,28 @@ namespace Packt.Shared
         public string Name;
         public DateTime DateOfBirth;
         public List<Person> Children = new List<Person>();
-        
+
         // methods
         public void WriteToConsole()
         {
             WriteLine($"{Name} was born on a {DateOfBirth:dddd}.");
+        }
+
+        // static method to "multiply"
+        public static Person Procreate(Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}"
+            };
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+            return baby;
+        }
+        // instance method to "multiply"
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
         }
     }
 }
--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Packt.Shared;
+using static System.Console;
 
 namespace PeopleApp
 {
@@ -11,6 +12,24 @@ namespace PeopleApp
             taylor.Name = "Taylor";
             taylor.DateOfBirth = new DateTime(1986, 12, 20);
             taylor.WriteToConsole();
+
+            var harry = new Person { Name = "Harry" };
+            var mary = new Person { Name = "Mary" };
+            var jill = new Person { Name = "Jill" };
+
+            // call instance method
+            var baby1 = mary.ProcreateWith(harry);
+            baby1.Name = "Gary";
+
+            // call static method
+            var baby2 = Person.Procreate(harry, jill);
+            WriteLine($"{harry.Name} has {harry.Children.Count} children.");
+            WriteLine($"{mary.Name} has {mary.Children.Count} children.");
+            WriteLine($"{jill.Name} has {jill.Children.Count} children.");
+            WriteLine(
+                format: "{0}'s first child is named \"{1}\".",
+                arg0: harry.Name,
+                arg1: harry.Children[0].Name);
         }
     }
 }
