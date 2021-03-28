2b95e34e3792ef09d44eaf1dc71d07538e33c64e
Taylor A. Liss <tayloraliss@gmail.com>
2021-03-28 00:56:07 -0400

    Add generic method

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -83,10 +83,22 @@ namespace PeopleApp
             var gt1 = new GenericThing<int>();
             gt1.Data = 42;
             WriteLine($"GenericThing with an integer: {gt1.Process(42)}");
-            
+
             var gt2 = new GenericThing<string>();
             gt2.Data = "apple";
             WriteLine($"GenericThing with a string: {gt2.Process("apple")}");
+
+            // generic method 
+            string number1 = "4";
+
+            WriteLine("{0} squared is {1}",
+                arg0: number1,
+                arg1: Squarer.Square<string>(number1));
+                byte number2 = 3;
+
+            WriteLine("{0} squared is {1}",
+                arg0: number2,
+                arg1: Squarer.Square(number2));
         }
 
         private static void Harry_Shout(object sender, EventArgs e)
