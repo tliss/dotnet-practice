df75a1c30a0bbb9871f852e5d5d3856c59d1ff89
Taylor A. Liss <tayloraliss@gmail.com>
2021-08-26 23:54:49 -0400

    Add chapter 7 files

---
+++ Chapter07/AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+using System.Xml.Linq;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doc = new XDocument();
+
+            // Keyword (better to use keyword over type because you don't need System namespace imported)
+            string s1 = "Hello";
+            // Type
+            String s2 = "World";
+
+            WriteLine($"{s1} {s2}");
+        }
+    }
+}
